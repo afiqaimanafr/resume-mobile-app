49278869dc7a722e3ddc0aac17cc667770fed557
afiqaimanafr <aaafr98@gmail.com>
2021-12-06 13:43:47 +0800

    latest ui

--- lib/screens/experience/experience_screen.dart
+++ lib/screens/experience/experience_screen.dart
@@ -1,18 +1,42 @@
 import 'package:afiq_resume/constants.dart';
+import 'package:afiq_resume/models/experience_model.dart';
 import 'package:flutter/material.dart';
 
 class ExperienceScreen extends StatelessWidget {
-  const ExperienceScreen({Key? key}) : super(key: key);
+  List<ExperienceModel> experience = [
+    ExperienceModel(
+      title: 'title',
+      subtitle: 'subtitle',
+      icons: const Icon(Icons.ac_unit),
+    ),
+    ExperienceModel(
+      title: 'title',
+      subtitle: 'subtitle',
+      icons: const Icon(Icons.ac_unit),
+    ),
+    ExperienceModel(
+      title: 'title',
+      subtitle: 'subtitle',
+      icons: const Icon(Icons.ac_unit),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: kPrimaryColor,
-      body: Center(
-        child: Text(
-          'Experience Screen',
-          style: TextStyle(color: kPrimaryLightColor),
-        ),
+      body: ListView.builder(
+        itemCount: experience.length,
+        itemBuilder: (ctx, index) {
+          final item = experience[index];
+          return Card(
+            child: ListTile(
+              leading: item.icons,
+              title: Text(item.title),
+              subtitle: Text(item.subtitle),
+            ),
+          );
+        },
       ),
     );
   }
