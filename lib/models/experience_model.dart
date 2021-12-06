723dead744d35c7ebe8b276a8d73f34bc4700301
afiqaimanafr <aaafr98@gmail.com>
2021-12-06 13:39:09 +0800

    latest ui

---
+++ lib/models/experience_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ExperienceModel {
+  final String title;
+  final String subtitle;
+  final Icon icons;
+
+  ExperienceModel({
+    required this.title,
+    required this.subtitle,
+    required this.icons,
+  });
+}
