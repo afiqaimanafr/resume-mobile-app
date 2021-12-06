705454e2e3931b6319f8ed4a629729162fc197a4
afiqaimanafr <aaafr98@gmail.com>
2021-12-06 14:56:39 +0800

    latest ui

--- lib/models/experience_model.dart
+++ lib/models/experience_model.dart
@@ -4,10 +4,12 @@ class ExperienceModel {
   final String title;
   final String subtitle;
   final String image;
+  final String firstDetail;
 
   ExperienceModel({
     required this.title,
     required this.subtitle,
     required this.image,
+    required this.firstDetail,
   });
 }
--- lib/screens/experience/experience_screen.dart
+++ lib/screens/experience/experience_screen.dart
@@ -5,17 +5,33 @@ import 'package:flutter/material.dart';
 class ExperienceScreen extends StatelessWidget {
   List<ExperienceModel> experience = [
     ExperienceModel(
-        title: 'Mobile Developer',
-        subtitle: 'October 2021 - Present',
-        image: 'assets/images/smartphone.png'),
+      title: 'Mobile Developer',
+      subtitle: 'October 2021 - Present',
+      image: 'assets/images/smartphone.png',
+      firstDetail:
+          'Developed mobile applications by utilizing Flutter and Dart',
+    ),
     ExperienceModel(
-        title: 'Web Developer Trainee',
-        subtitle: 'July 2021 - September 2021',
-        image: 'assets/images/web-programming.png'),
+      title: 'Web Developer Trainee',
+      subtitle: 'July 2021 - September 2021',
+      image: 'assets/images/web-programming.png',
+      firstDetail:
+          'Assisted in carrying out both frontend and backend development activities and harnessed an array of tools including HTML, CSS, JS, PHP and Laravel',
+    ),
     ExperienceModel(
-        title: 'Part-time Worker',
-        subtitle: 'June 2019 - August 2019',
-        image: 'assets/images/cooking.png'),
+      title: 'Part-time Worker',
+      subtitle: 'June 2019 - August 2019',
+      image: 'assets/images/cooking.png',
+      firstDetail:
+          'Performed food preparation and cashiering activities in ensuring excellent customer service',
+    ),
+    ExperienceModel(
+      title: 'Freelance Tutor',
+      subtitle: 'June 2018 - Present',
+      image: 'assets/images/tutor.png',
+      firstDetail:
+          'Tutored the subjects of Mathematics and Science to PT3 and SPM level students',
+    ),
   ];
 
   @override
@@ -27,7 +43,10 @@ class ExperienceScreen extends StatelessWidget {
         itemBuilder: (ctx, index) {
           final item = experience[index];
           return Container(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 5,
+            ),
             width: double.infinity,
             child: Card(
               color: Colors.white.withOpacity(0.2),
@@ -59,11 +78,21 @@ class ExperienceScreen extends StatelessWidget {
                             item.title,
                             style: const TextStyle(
                               color: kPrimaryLightColor,
-                              fontFamily: secondaryFamilyFont,
+                              fontFamily: primaryFamilyFont,
                             ),
                           ),
                           Text(
                             item.subtitle,
+                            style: const TextStyle(
+                              color: kPrimaryLightColor,
+                              fontFamily: primaryFamilyFont,
+                            ),
+                          ),
+                          const Divider(
+                            color: kPrimaryLightColor,
+                          ),
+                          Text(
+                            item.firstDetail,
                             style: const TextStyle(
                               color: kPrimaryLightColor,
                               fontFamily: secondaryFamilyFont,
