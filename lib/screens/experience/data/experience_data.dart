557458881b380825a329803a81c8125fb308c081
afiqaimanafr <aaafr98@gmail.com>
2021-12-08 09:19:31 +0800

    latest ui

---
+++ lib/screens/experience/data/experience_data.dart
@@ -0,0 +1,32 @@
+import 'package:afiq_resume/models/experience_model.dart';
+
+final allExperience = <ExperienceModel>[
+  ExperienceModel(
+    title: 'Mobile Developer',
+    subtitle: 'October 2021 - Present',
+    image: 'assets/images/smartphone.png',
+    firstDetail:
+        'Successfully developed two mobile applications (as of now) by utilizing Flutter and Dart',
+  ),
+  ExperienceModel(
+    title: 'Web Developer Trainee',
+    subtitle: 'July 2021 - September 2021',
+    image: 'assets/images/web-programming.png',
+    firstDetail:
+        'Assisted in carrying out both frontend and backend development activities and harnessed an array of tools including HTML, CSS, JS, PHP and Laravel',
+  ),
+  ExperienceModel(
+    title: 'Part-time Worker',
+    subtitle: 'June 2019 - August 2019',
+    image: 'assets/images/cooking.png',
+    firstDetail:
+        'Performed food preparation and cashiering activities in ensuring excellent customer service',
+  ),
+  ExperienceModel(
+    title: 'Freelance Tutor',
+    subtitle: 'June 2018 - Present',
+    image: 'assets/images/tutor.png',
+    firstDetail:
+        'Tutored the subjects of Mathematics and Science to PT3 and SPM level students',
+  ),
+];
--- lib/screens/experience/experience_screen.dart
+++ lib/screens/experience/experience_screen.dart
@@ -1,38 +1,21 @@
 import 'package:afiq_resume/constants.dart';
 import 'package:afiq_resume/models/experience_model.dart';
+import 'package:afiq_resume/screens/experience/data/experience_data.dart';
 import 'package:flutter/material.dart';
 
-class ExperienceScreen extends StatelessWidget {
-  List<ExperienceModel> experience = [
-    ExperienceModel(
-      title: 'Mobile Developer',
-      subtitle: 'October 2021 - Present',
-      image: 'assets/images/smartphone.png',
-      firstDetail:
-          'Successfully developed two mobile applications (as of now) by utilizing Flutter and Dart',
-    ),
-    ExperienceModel(
-      title: 'Web Developer Trainee',
-      subtitle: 'July 2021 - September 2021',
-      image: 'assets/images/web-programming.png',
-      firstDetail:
-          'Assisted in carrying out both frontend and backend development activities and harnessed an array of tools including HTML, CSS, JS, PHP and Laravel',
-    ),
-    ExperienceModel(
-      title: 'Part-time Worker',
-      subtitle: 'June 2019 - August 2019',
-      image: 'assets/images/cooking.png',
-      firstDetail:
-          'Performed food preparation and cashiering activities in ensuring excellent customer service',
-    ),
-    ExperienceModel(
-      title: 'Freelance Tutor',
-      subtitle: 'June 2018 - Present',
-      image: 'assets/images/tutor.png',
-      firstDetail:
-          'Tutored the subjects of Mathematics and Science to PT3 and SPM level students',
-    ),
-  ];
+class ExperienceScreen extends StatefulWidget {
+  @override
+  State<ExperienceScreen> createState() => _ExperienceScreenState();
+}
+
+class _ExperienceScreenState extends State<ExperienceScreen> {
+  late List<ExperienceModel> experience;
+
+  @override
+  void initState() {
+    super.initState();
+    experience = allExperience;
+  }
 
   @override
   Widget build(BuildContext context) {
